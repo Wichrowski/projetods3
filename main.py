b15c9a7558b35ee9c11ac198784a24e59f08959f
Wichrowski <joseane.wichrowski@hotmail.com>
2019-05-26 21:11:28 -0300

    Alterações na main, requirements e criando pasta Templates com arquivo login_parceiro.html

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def login_parceiro():
+    return render_template("login_parceiro.html")
