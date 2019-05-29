4dfc11d6805e69d56116d42b338263cafe06b6c4
Wichrowski <joseane.wichrowski@hotmail.com>
2019-05-29 12:35:09 -0300

    Alterando main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask import render_template
+from flask_login import LoginManager
 app = Flask(__name__)
+login_manager = LoginManager()
 
-@app.route("/")
+@app.route("/pagina-inicial")
 def login_parceiro():
     return render_template("login_parceiro.html")
+
+@app.route("/eventos")
+def eventos():
+    return render_template("eventos.html")
