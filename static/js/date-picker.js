912d658004ab1591bb35b348b2f9bf2eafd74570
Wichrowski <joseane.wichrowski@hotmail.com>
2019-06-04 00:53:43 -0300

    Datepicker

---
+++ static/js/date-picker.js
@@ -0,0 +1,33 @@
+function datePicker(selector) {
+
+  var field = document.querySelector(selector);
+
+  console.log(field);
+
+  var picker = new Pikaday({
+    field: document.querySelector(selector),
+    format: 'DD/MM/YYYY',
+    i18n: {
+      previousMonth: 'Mês anterior',
+      nextMonth: 'Próximo mês',
+      months: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+      weekdays: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+      weekdaysShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab']
+    },
+    toString: function(date, format) {
+      var day = date.getDate();
+      var month = date.getMonth() + 1;
+      var year = date.getFullYear();
+
+      day = day < 10
+        ? '0' + day
+        : day;
+
+      month = month < 10
+        ? '0' + month
+        : month;
+
+      return `${day}/${month}/${year}`;
+    }
+  })
+}
